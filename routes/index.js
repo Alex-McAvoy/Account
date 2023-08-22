d8c902ed0d1e9e8c4df4e950c74e22907401f2c1
Alex-McAvoy <931772462@qq.com>
2023-08-23 01:27:38 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,22 +4,33 @@
  * @Date: 2023-08-22 21:24:16
  */
 var express = require('express');
+var lowdb = require('lowdb');
+var fileSync = require('lowdb/adapters/FileSync');
+var shortid = require('shortid');
+
+// 创建路由对象
 var router = express.Router();
 
+// 创建 db 对象
+var adapter = new fileSync(__dirname + '/../data/db.json');
+var db = lowdb(adapter);
+
 // 记账本列表页
-router.get('/account', function(req, res, next) {
+router.get('/account', function (req, res, next) {
   res.render('list');
 });
 
 // 添加记录页
-router.get('/account/create', function(req, res, next) {
+router.get('/account/create', function (req, res, next) {
   res.render('create');
 });
 
 // 添加记录
-router.post('/account', function(req, res, next) {
-  // 获取请求体数据
-  console.log(req.body)
+router.post('/account', function (req, res, next) {
+  // 生成 id
+  let id = shortid.generate();
+  // 请求体数据写入 lowdb
+  db.get('accounts').unshift({ id: id, ...req.body }).write();
   res.send('添加记录');
 });
 
