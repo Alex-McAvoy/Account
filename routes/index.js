2212355bdea7876e322a2f61e985e08cdea11af2
Alex-McAvoy <931772462@qq.com>
2023-08-23 01:18:12 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,21 @@
 var express = require('express');
 var router = express.Router();
 
-// 记账本列表
+// 记账本列表页
 router.get('/account', function(req, res, next) {
   res.render('list');
 });
 
-// 添加记录
+// 添加记录页
 router.get('/account/create', function(req, res, next) {
   res.render('create');
+});
+
+// 添加记录
+router.post('/account', function(req, res, next) {
+  // 获取请求体数据
+  console.log(req.body)
+  res.send('添加记录');
 });
 
 module.exports = router;
