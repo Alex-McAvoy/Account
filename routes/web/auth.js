d8819fd1aef543bbf77ccbe660c48d3bb741d7ef
Alex-McAvoy <931772462@qq.com>
2023-08-24 03:17:36 +0800

    Update auth.js

--- routes/web/auth.js
+++ routes/web/auth.js
@@ -4,6 +4,8 @@
  * @Date: 2023-08-24 02:27:25
  */
 var express = require('express');
+var md5 = require('md5');
+var UserModel = require('../../models/UserModel');
 
 var router = express.Router();
 
@@ -15,6 +17,36 @@ router.get('/reg', (req, res) => {
 // 登录页面
 router.get('/login', (req, res) => {
     res.render('auth/login');
+});
+
+// 注册操作
+router.post('/reg', (req, res) => {
+    // 将 password 进行 md5 加密
+    req.body.password = md5(req.body.password);
+    // 创建新用户
+    UserModel.create({
+        ...req.body
+    }).then(data => {
+        return res.render('success', { msg: ':)注册成功', url: '/login' });
+    }).catch(error => {
+        return res.status(500).send('注册失败' + error);
+    });
+});
+
+// 登录操作
+router.post('/login', (req, res) => {
+    // 获取用户名密码
+    let { username, password } = req.body;
+    // 查询数据库
+    UserModel.findOne({ username: username, password: md5(password) }).then(data => {
+        if (!data) {
+            return res.send('账户或密码错误')
+        } else {
+            return res.render('success', { msg: ':)登录成功', url: '/account' });
+        }
+    }).catch(error => {
+        return res.status(500).send('登录失败' + error);
+    });
 });
 
 module.exports = router;
