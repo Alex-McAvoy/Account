34ae7854f8192cfbdd9e332b9ed0e5f70cef7524
Alex-McAvoy <931772462@qq.com>
2023-08-24 19:24:45 +0800

    Update CheckTokenMiddleware.js

--- middleware/CheckTokenMiddleware.js
+++ middleware/CheckTokenMiddleware.js
@@ -4,8 +4,10 @@
  * @Date: 2023-08-24 19:15:23
  */
 const jwt = require('jsonwebtoken');
+const { secret } = require('../config/config');
 
 module.exports = (req, res, next) => {
+    // 检测 token
     let token = req.get('token');
     if (!token) {
         return res.json({
@@ -16,7 +18,7 @@ module.exports = (req, res, next) => {
     }
 
     // 校验 token
-    jwt.verify(token, 'alex', (error, data) => {
+    jwt.verify(token, secret, (error, data) => {
         if (error) {
             return res.json({
                 code: '20003',
@@ -24,6 +26,9 @@ module.exports = (req, res, next) => {
                 error: error
             });
         }
+        // 保存用户信息
+        req.user = data;
+        // 校验通过
         next();
     });
 }
