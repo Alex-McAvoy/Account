7c5a3437425a5e5983ed63e17d0a46d2569995bf
Alex-McAvoy <931772462@qq.com>
2023-08-24 19:24:16 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,10 +1,11 @@
 /*
- * @Description: 数据库配置文件
+ * @Description: 配置文件
  * @Author: Alex_McAvoy
  * @Date: 2023-08-23 19:09:30
  */
 module.exports = {
   DB_HOST: '127.0.0.1',
   DB_PORT: 27017,
-  DB_NAME: 'account'
+  DB_NAME: 'account',
+  secret: 'alex'
 }
